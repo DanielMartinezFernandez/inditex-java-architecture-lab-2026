02724ed6173e7f1fb06dd9b0d77d821698c93a29
DanielMartinezFernandez <31846613+danielmartinezfernandez@users.noreply.github.com>
2026-01-20 23:17:59 +0100

    update imports

--- src/main/java/com/inditex/lab/infrastructure/adapter/in/web/ProductPriceController.java
+++ src/main/java/com/inditex/lab/infrastructure/adapter/in/web/ProductPriceController.java
@@ -5,7 +5,11 @@ import jakarta.validation.constraints.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
